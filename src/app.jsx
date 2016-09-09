1230e5125bfe6bb6aa88f905c25653294325436d
Bart van Dorp (Henk) <henk@raadhuis.com>
2016-09-09 11:46:55 +0200

    removed bootstrap and installed foundation
    Signed-off-by: Bart van Dorp (Henk) <henk@raadhuis.com>

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'foundation-sites/dist/foundation.min.css';
 import styles from './index.scss';
 import React from 'react';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="small-4 small-offset-4">
         <h1>It Works!</h1>
         <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
         <p>Global bootstrap css import works too as you can see on the following button.</p>
-        <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
+        <p><a className="button">Enjoy!</a></p>
       </div>
     )
   }
